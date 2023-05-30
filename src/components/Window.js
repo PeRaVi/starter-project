fbc82e7752365459ce3733b63aee8f8b74c11b6d
Pedro Ramallo Vidal <pedroramallov@gmail.com>
2023-05-31 00:44:32 +0200

    Window movement

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,18 +1,52 @@
 import '../styles/Window.css';
+import RedButton from './RedButton';
+import YellowButton from './YellowButton';
+import GreenButton from './GreenButton';
+import { useState, useEffect } from 'react';
+
 const Window = () => {
-    const closeButton = () => {
-      const windowElement = document.querySelector('.window');
-      if (windowElement) {
-        windowElement.style.display = 'none';
-      }
-    };
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isDragging, setIsDragging] = useState(false);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    if (isDragging) {
+      const handleMouseMove = (event) => {
+        const newX = event.clientX - offset.x;
+        const newY = event.clientY - offset.y;
+        setPosition({ x: newX, y: newY });
+      };
+
+      const handleMouseUp = () => {
+        setIsDragging(false);
+      };
+
+      document.addEventListener('mousemove', handleMouseMove);
+      document.addEventListener('mouseup', handleMouseUp);
+
+      return () => {
+        document.removeEventListener('mousemove', handleMouseMove);
+        document.removeEventListener('mouseup', handleMouseUp);
+      };
+    }
+  }, [isDragging, offset]);
+
+  const handleMouseDown = (event) => {
+    setIsDragging(true);
+    setOffset({ x: event.clientX - position.x, y: event.clientY - position.y });
+  };
+
   return (
-    <div className="window">
+    <div className="window"
+    style={{ top: position.y, left: position.x }}
+    onMouseDown={handleMouseDown}
+    >
     <div className="window-bar">
       <div className="window-controls">
-        <button onClick={closeButton} className="window-control"></button>
-        <button className="window-control"></button>
-        <button className="window-control"></button>
+        <RedButton/>
+        <YellowButton/>
+        <GreenButton/>
       </div>
     </div>
     <div className="window-content">
