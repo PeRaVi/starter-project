53f607fc6b7cbb8e906db2d919896ae0fde03055
Pedro Ramallo Vidal <pedroramallov@gmail.com>
2023-05-31 05:39:05 +0200

    menuItem into navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,28 @@
 
 import '../styles/Navbar.css';
 import Clock from './Clock';
+import MenuItem from './MenuItem';
 
 const Navbar = () => {
+
+  const aboutItems = ['Aux 1', 'Aux 2', 'Aux 3'];
+  const proyectItems = ['Aux 1', 'Aux 2', 'Aux 3'];
+  const contactItems = ['Aux 1', 'Aux 2', 'Aux 3'];
+
   return (
     <div className="navbar">
       <ul className="menu-list">
-        <li className="menu-item">
-          <div className="menu-text">Sobre mí</div>
-        </li>
-        <li className="menu-item">
-         <div className="menu-text">Proyectos</div>
-        </li>
-        <li className="menu-item">
-          <div className="menu-text">Contacto</div>
-        </li>
+        <MenuItem value="Sobre mí" menuItems={aboutItems} dropdown={true}/>
+        <MenuItem value="Proyectos" menuItems={proyectItems} dropdown={true}/>
+        <MenuItem value="Contactos" menuItems={contactItems} dropdown={true}/>
       </ul>
       
       <ul className="menu-state">
       
-      <li className="menu-item"> 
-        
-        <div className="icon-containerv">
-          <img 
-             className="menu-icon" 
-            src={process.env.PUBLIC_URL + '/icons/rain.ico'} 
-            alt="Orballo" />
-        </div>
-        <div className="menu-text">
-          A Coruña
-        </div> 
-     
-     
-        
-      </li>
+      <MenuItem value="A Coruña" icon={"rain.ico"} iconAlt={"Orballo"}/>
 
-      <li className="menu-item"> 
-      <div className="menu-text"><Clock/> </div>
-      
-      </li>
+      <MenuItem value={<Clock/>} />
+
         
       </ul>
 
